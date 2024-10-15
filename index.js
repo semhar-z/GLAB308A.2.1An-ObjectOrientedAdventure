e35203993be1b273a0799e699f1b1ba0329e6b08
semhar-z <semharzewoldi@gmail.com>
2024-10-15 11:19:20 -0400

    part 2 is done

--- index.js
+++ index.js
@@ -1,37 +1,71 @@
 // Part 1: Humble Beginnings
 
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"],
     
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        companion: {  // Leo's companion (Frank) and its own belongings
-            name: "Frank",
-            type: "Flea",
-            belongings: ["small hat", "sunglasses"]
-        }
+//     companion: {
+//         name: "Leo",
+//         type: "Cat",
+//         companion: {  // Leo's companion (Frank) and its own belongings
+//             name: "Frank",
+//             type: "Flea",
+//             belongings: ["small hat", "sunglasses"]
+//         }
 
-    },
+//     },
+
+//     roll (mod = 0) {
+//         const result = Math.floor(Math.random() * 20) + 1 + mod;
+//         console.log(`${this.name} rolled a ${result}.`)
+//         }
+
+//     }   
+
+//     // create a loop that logs each item in Robin’s inventory.
+
+// adventurer.inventory.forEach(item => {
+//     console.log(item);
+// });
+
+// adventurer.roll()
+// adventurer.roll()
+// adventurer.roll()
+// console.log(adventurer.roll());
+
+
+// Part 2: Class Fantasy
+
+// Start with a Character class, which will define generic character entities.
+
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
 
     roll (mod = 0) {
-        const result = Math.floor(Math.random() * 20) + 1 + mod;
-        console.log(`${this.name} rolled a ${result}.`)
-        }
+                const result = Math.floor(Math.random() * 20) + 1 + mod;
+                console.log(`${this.name} rolled a ${result}.`)
+                }
 
-    }   
+  }
 
-    // create a loop that logs each item in Robin’s inventory.
 
-adventurer.inventory.forEach(item => {
-    console.log(item);
-});
+  const robin = new Character("Robin");
+  robin.inventory = ["sword", "potion", "artifact"];
+  robin.companion = new Character("Leo");
+  robin.companion.type = "Cat";
+  robin.companion.companion = new Character("Frank");
+  robin.companion.companion.type = "Flea";
+  robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-adventurer.roll()
-adventurer.roll()
-adventurer.roll()
-console.log(adventurer.roll());
+robin.roll();                        
+robin.companion.roll();            
+robin.companion.companion.roll();     
 
+
+//  Part 3: Class Features
 
